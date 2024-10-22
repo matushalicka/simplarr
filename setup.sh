4377eee39035d7f1fbd9ff0eaa33d6d6a59e1987
Matus Halicka <matus.halicka@gmail.com>
2024-10-22 20:29:54 +0200

    sabnzbd added, latest version of qbittorrent is not used because of issues, misc

--- setup.sh
+++ setup.sh
@@ -3,17 +3,17 @@
 source .env
 
 # Make directories
-sudo mkdir -pv ./configs/linuxserver/{jellyfin,radarr,sonarr,prowlarr,qbittorrent,bazarr}
-sudo mkdir -pv ${MOVIES_DIRECTORY}/data/movies/{torrents,media}
-sudo mkdir -pv ${TV_SHOWS_DIRECTORY}/data/tvshows/{torrents,media}
+sudo mkdir -pv ./configs/linuxserver/{jellyfin,radarr,sonarr,prowlarr,sabnzbd,qbittorrent,bazarr}
+sudo mkdir -pv ${MOVIES_DIRECTORY}/data/movies/{media,nzbs,torrents}
+sudo mkdir -pv ${TV_SHOWS_DIRECTORY}/data/tvshows/{media,nzbs,torrents}
 
 # Set permissions
 sudo chmod 775 ./configs
 sudo chown ${USER_ID}:${GROUP_ID} ./configs
 sudo chmod 775 ./configs/linuxserver
 sudo chown ${USER_ID}:${GROUP_ID} ./configs/linuxserver
-sudo chmod 775 ./configs/linuxserver/{jellyfin,radarr,sonarr,prowlarr,qbittorrent,bazarr}
-sudo chown ${USER_ID}:${GROUP_ID} ./configs/linuxserver/{jellyfin,radarr,sonarr,prowlarr,qbittorrent,bazarr}
+sudo chmod 775 ./configs/linuxserver/{jellyfin,radarr,sonarr,prowlarr,sabnzbd,qbittorrent,bazarr}
+sudo chown ${USER_ID}:${GROUP_ID} ./configs/linuxserver/{jellyfin,radarr,sonarr,prowlarr,sabnzbd,qbittorrent,bazarr}
 
 sudo chmod 775 ${MOVIES_DIRECTORY}
 sudo find ${MOVIES_DIRECTORY} -type d -exec sudo chmod 775 {} \;
