e0b7f7bb9407bc1329bb303e9422ec3667dc16d2
Matúš Halička <matus.halicka@gmail.com>
2025-08-26 22:21:35 +0200

    data2 added

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source .env
 sudo mkdir -pv ./configs/{bazarr,jellyfin,prowlarr,qbittorrent,radarr,recyclarr,sabnzbd,sonarr}
 sudo mkdir -pv "$DATA_DIRECTORY"/{media,torrents,usenet}/{movies,tv}
 sudo mkdir -pv "$DATA_DIRECTORY"/{torrents,usenet}/{incomplete,others}
+sudo mkdir -pv ./data2/{torrents,usenet}/{incomplete,movies,others,tv}
 
 # Set permissions
 sudo chmod 775 ./configs
@@ -15,3 +16,7 @@ sudo chown $USER_ID:$GROUP_ID ./configs/{bazarr,jellyfin,prowlarr,qbittorrent,ra
 sudo find "$DATA_DIRECTORY" -type d -exec sudo chmod 775 {} \;
 sudo find "$DATA_DIRECTORY" -type f -exec sudo chmod 664 {} \;
 sudo chown -R $USER_ID:$GROUP_ID "$DATA_DIRECTORY"
+
+sudo find ./data2 -type d -exec sudo chmod 775 {} \;
+sudo find ./data2 -type f -exec sudo chmod 664 {} \;
+sudo chown -R $USER_ID:$GROUP_ID ./data2
